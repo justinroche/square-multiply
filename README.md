bbf97f2df709a9c050a01c83ff3b2f526b6bb2e5
justinroche <justinroche03@gmail.com>
2024-03-14 11:28:55 -0500

    Build algorithm

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # square-multiply
+
 A square and multiply algorithm implemented in C#
+
+1. **Restore Dependencies**: In the terminal, navigate to the project directory and run the following command:
+
+   ```
+   dotnet restore
+   ```
+
+2. **Build the Project**: Once dependencies are restored, you can build the project using the `dotnet build` command:
+
+   ```
+   dotnet build
+   ```
+
+3. **Run the Application**: If the build is successful, you can run the application.
+   ```
+   dotnet run
+   ```
---
+++ SquareMultiply/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+
+  static void Main()
+  {
+
+    // Obtain modular exponentiation values.
+    Console.WriteLine("Enter the base number: ");
+    int baseNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the exponent: ");
+    int exponent = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter the modulo: ");
+    int modulo = int.Parse(Console.ReadLine());
+
+    // Compute and print result.
+    int result = RunSquareMultiply(baseNumber, exponent, modulo);
+    Console.WriteLine("Result: " + result);
+  }
+
+  static int RunSquareMultiply(int baseNumber, int exponent, int modulo)
+  {
+    int result = 1;
+    string binaryExponent = Convert.ToString(exponent, 2);
+
+    Console.WriteLine("Exponent (" + exponent + ") in binary: " + binaryExponent);
+
+    for (int i = 0; i < binaryExponent.Length; i++)
+    {
+      result = result * result % modulo;
+      if (binaryExponent[i] == '1')
+        result = result * baseNumber % modulo;
+
+      Console.WriteLine("Step " + i + " (" + binaryExponent[i] + "): " + result);
+    }
+
+    return result;
+  }
+
+}
